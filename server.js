7c9bc47a6ef04dac3649e972d5a5f61839b779a3
Aaditya Singh <singhaditya2763@gmail.com>
2024-04-14 15:07:15 +0530

    update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
     'https://tags-footwear-admin.vercel.app',
     'https://tags-website-frontend.vercel.app',
     `http://103.169.86.3:5173`,
-`http://103.169.86.3:537`
+`http://103.169.86.3:5174`
 ];
 
 // Define CORS middleware configuration
