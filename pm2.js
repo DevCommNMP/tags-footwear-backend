6767b3e4e1afa7aeedf09bd1a63bc94f42fd1091
Aaditya Singh <singhaditya2763@gmail.com>
2024-05-06 11:13:38 +0530

    update

---
+++ pm2.js
@@ -0,0 +1,21 @@
+ecosystem.config.js
+
+
+module.exports = {
+  apps: [
+    {
+      name: "tags-backend",
+      script: "./app.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
