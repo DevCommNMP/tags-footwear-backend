d85381cc7567f7a3cd09ad1ce16d30505054617f
Aaditya Singh <singhaditya2763@gmail.com>
2024-05-27 12:45:13 +0530

    update

--- config/multerConfig/productImageUpload.js
+++ config/multerConfig/productImageUpload.js
@@ -27,7 +27,7 @@ const fileFilter = (req, file, cb) => {
 // Initialize Multer with configuration
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 1024 * 1024 * 5 }, // Limit file size to 5MB
+  limits: { fileSize: 1024 * 1024 * 25 }, // Limit file size to 5MB
   fileFilter: fileFilter
 });
 
