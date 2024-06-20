178586b5039ed783da443f383c1691a81c03a832
Aaditya Singh <singhaditya2763@gmail.com>
2024-06-20 12:52:09 +0530

    update

--- controller/checkoutController/checkoutControllers.js
+++ controller/checkoutController/checkoutControllers.js
@@ -14,6 +14,13 @@ const Product = require("../../modals/product/product");
 var email = process.env.courierEmail;
 var password = process.env.courierPassword;
 
+let OrderData;
+let BillingData;
+let Amount;
+let Quantity;
+let ProductName;
+let ProductCode;
+let ProductData;
 // Concatenate email and password with a colon
 var credentials = email + ":" + password;
 
@@ -44,112 +51,7 @@ const getKeys = async (req, res) => {
     res.status(201).json({ message: "something went wrong try again" });
   }
 };
-// const codCheckout = async (req, res) => {
-//   try {
-//       const formData = req.body.formData;
-//       const amount = req.body.amount;
-//       const email = req.body.userEmail;
-//       const { CGST, SGST, Tax } = req.body;
-//       const user = await User.findOne({ email });
 
-//       const placedOrder = await Order.create({
-//           user: user.id,
-//           orderId: generateOrderId(),
-//       });
-
-//       const products = req.body.cartdata.map((item) => ({
-//           product: item.productId,
-//           quantity: item.quantity,
-//           price: item.price,
-//           size: item.size,
-//           color: item.color,
-//       }));
-
-//       const orderDetails = await OrderDetail.create({
-//           orderId: placedOrder.orderId,
-//           productDetails: products,
-//           billingDetails: {
-//               fname: formData.fname,
-//               lname: formData.lname,
-//               billing_address: formData.billing_address,
-//               billing_address2: formData.billing_address2,
-//               city: formData.city,
-//               zipcode: formData.zipcode,
-//               phone: formData.phone,
-//               state: formData.state,
-//               email: formData.email,
-//               additionalInfo: formData.additionalInfo,
-//           },
-//           subtotal: req.body.amount,
-//           CGST: req.body.CGST,
-//           SGST: req.body.SGST,
-//           totalTax: req.body.Tax,
-//       });
-
-//       const orderdata = await Order.findOne({ orderId: placedOrder.orderId });
-//       const OrderDetaitlsData = await OrderDetail.findOne({ orderId: orderdata.orderId });
-//       const courierPromises = req.body.cartdata.map(async (item) => {
-//           return axios.post('https://api.tekipost.com/connect/order-ship', {
-//               "order_no": generateOrderId(),
-//               "customer_name": formData.fname + ' ' + formData.lname,
-//               "customer_address_1": formData.billing_address,
-//               "customer_address_2": formData.billing_address2,
-//               "customer_pincode": formData.zipcode,
-//               "customer_city": formData.city,
-//               "customer_state": formData.state,
-//               "customer_mobile_no": formData.phone,
-//               "customer_alt_no": "",
-//               "customer_email": formData.email,
-//               "product_category": "Fashion & lifestyle",
-//               "product_code": item.productCode,
-//               "product_name": item.product,
-//               "product_qty": item.quantity,
-//               "invoice_value": item.price,
-//               "cod_value": "1",
-//               "weight_in_kgs": "0.5",
-//               "length_in_cms": "10",
-//               "breadth_in_cms": "10",
-//               "height_in_cms": "10",
-//               "warehouse_id": "1",
-//               "movement_type": "Forward",
-//               "store_id": "",
-//               "courier_id": "",
-//               "custom_awb_no": "",
-//               "service_name": "Surface",
-//           }, {
-//               headers: {
-//                   'Authorization': `Bearer ${encodedCredentials}`
-//               }
-//           });
-//       });
-
-//       try {
-//           const responses = await Promise.all(courierPromises);
-//           console.log(responses); // Log all responses
-//           // Handle responses if needed
-//       } catch (error) {
-//           console.error("Error sending courier data:", error);
-//           // Handle error if necessary
-//       }
-
-//       const modifyOrderData = await Order.findOneAndUpdate(
-//           { orderId: placedOrder.orderId },
-//           { $set: { orderDetails: OrderDetaitlsData._id } },
-//           { new: true }
-//       );
-
-//       const updateUserData= await User.findOneAndUpdate(
-//           { email: email },
-//           { $push: { order: modifyOrderData._id } },
-//           { new: true }
-//       );
-
-//       res.status(200).json({ success: true, modifyOrderData });
-//   } catch (error) {
-//       console.log(error.message);
-//       res.status(201).json({ errorMsg: error.message, success: false });
-//   }
-// }
 
 const codCheckout = async (req, res) => {
   try {
@@ -358,6 +260,7 @@ const codCheckout = async (req, res) => {
     const updatedProducts = await Promise.all(productmodification);
 
     console.log("products updated successfully");
+    res.status(201).json({ message:"order Placed Successfully" , success: true });
     // Do something with updatedProducts if needed
   } catch (error) {
     console.log(error.message);
@@ -367,10 +270,15 @@ const codCheckout = async (req, res) => {
 
 const checkout = async (req, res) => {
   const formData = req.body.formData;
+  BillingData=formData;
+
   const amount = req.body.amount;
   const email = req.body.userEmail;
   const { CGST, SGST, Tax } = req.body;
-  console.log(formData, amount, email, CGST, SGST, Tax);
+
+  Amount=amount;
+
+  // console.log(formData, amount, email, CGST, SGST, Tax);
   let productName = "";
   let productCode = "";
   let quantity = 0;
@@ -391,9 +299,13 @@ const checkout = async (req, res) => {
 
     await req.body.cartdata.forEach((item) => {
       productName += item.title + ",";
-      productCode += item.productCode + ",";
-      quantity += item.quantity; // Accumulate quantity
+      productCode += item.product.productName+","
+      quantity += item.quantity; 
+      Quantity=quantity// Accumulate quantity
+      ProductName=productName;
+      ProductCode =productCode
     });
+    ProductData=req.body.cartdata;
     // console.log("placedOrder",placedOrder)
     const products = await req.body.cartdata.map((item) => ({
       product: item.productId,
@@ -425,76 +337,12 @@ const checkout = async (req, res) => {
     });
 
     const orderdata = await Order.findOne({ orderId: placedOrder.orderId });
+    OrderData=orderdata;
     // console.log("====================", orderdata);
     const OrderDetaitlsData = await OrderDetail.findOne({
       orderId: orderdata.orderId,
     });
-    // console.log("00000000000000000000000",OrderDetaitlsData)
-    const courierRequest = async () => {
-      try {
-        const response = await axios.post(
-          "https://api.tekipost.com/connect/order-ship",
-          {
-            // invoice_no:order
-            order_no: orderdata.orderNumber, // Change to orderId
-            customer_name: formData.fname + " " + formData.lname,
-            customer_address_1: formData.billing_address,
-            customer_address_2: formData.billing_address2,
-            customer_pincode: formData.zipcode,
-            customer_city: formData.city,
-            customer_state: formData.state,
-            customer_mobile_no: formData.phone,
-            customer_alt_no: "",
-            customer_email: formData.email,
-            product_category: "Fashion & lifestyle",
-            product_code: productCode,
-            product_name: productName,
-            product_qty: quantity, // Use accumulated quantity
-            invoice_value: amount,
-            cod_value: "0",
-            weight_in_kgs: `${0.8 * quantity}`,
-            length_in_cms: `${28 * quantity}`,
-            breadth_in_cms: `${14 * quantity}`,
-            height_in_cms: `${28 * quantity}`,
-            warehouse_id: "589",
-            movement_type: "Forward",
-            store_id: "59",
-            courier_id: "",
-            custom_awb_no: "",
-            service_name: "Surface",
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${encodedCredentials}`,
-            },
-          }
-        );
-
-        return response.data;
-        console.log("orderplaced on tekipost");
-      } catch (error) {
-        throw error;
-      }
-    };
-    try {
-      const response = await courierRequest();
-      console.log(response);
-      if (!response.error) {
-        const modifyOrderData = await Order.findOneAndUpdate(
-          { orderId: placedOrder.orderId },
-          {
-            $set: {
-              order_id: response.order_id,
-              orderDetails: OrderDetaitlsData._id, 
-            },
-          },
-          { new: true }
-        );
-      }
-      // console.log(orderplaced);
-    } catch (error) {
-      console.error("Error in courierRequest:", error.message);
-    }
+    
 
     const modifyOrderData = await Order.findOneAndUpdate(
       { orderId: placedOrder.orderId },
@@ -507,8 +355,110 @@ const checkout = async (req, res) => {
       { new: true }
     );
 
-    const productmodification = await Promise.all(
-      req.body.cartdata.map(async (item) => {
+   
+    res.status(200).json({ success: true, order });
+  } catch (error) {
+    res
+      .status(403)
+      .json({ message: "something went wrong try again", succes: false });
+  }
+};
+
+const pincodeData = (req, res) => {
+  const { pincode } = req.body;
+  //console.log(pincode);
+  const data = query.search(pincode);
+  // console.log(data);
+  if (data.length == 0) {
+    res.status(201).json({ success: false });
+    return;
+  }
+  res.status(200).json({ data, success: true });
+};
+
+const paymentVerification = async (req, res) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_SECRET)
+    .update(body)
+    .digest("hex");
+
+  const isAuthentic = expectedSignature === razorpay_signature;
+
+  if (isAuthentic) {
+    try {
+      // Create payment record
+      await Payment.create({
+        razorpay_order_id,
+        razorpay_payment_id,
+        razorpay_signature,
+      });
+
+      // Update order payment status
+      const orderData = await Order.findOneAndUpdate(
+        { razorpay_order_id },
+        {
+          $set: {
+            PaymentStatus: "Paid",
+            paymentDetails: {
+              razorpay_order_id,
+              razorpay_payment_id,
+              razorpay_signature,
+            },
+          },
+        },
+        { new: true }
+      );
+
+      // Prepare data for courier request
+      const courierData = {
+        order_no: OrderData.orderNumber, // Change to orderId
+            customer_name: BillingData.fname + " " + BillingData.lname,
+            customer_address_1: BillingData.billing_address,
+            customer_address_2: BillingData.billing_address2,
+            customer_pincode: BillingData.zipcode,
+            customer_city: BillingData.city,
+            customer_state: BillingData.state,
+            customer_mobile_no: BillingData.phone,
+            customer_alt_no: "",
+            customer_email: BillingData.email,
+            product_category: "Fashion & lifestyle",
+            product_code: ProductCode,
+            product_name: ProductName,
+            product_qty: Quantity, // Use accumulated quantity
+            invoice_value: Amount,
+            cod_value: "0",
+            weight_in_kgs: `${0.8 * Quantity}`,
+            length_in_cms: `${28 * Quantity}`,
+            breadth_in_cms: `${14 * Quantity}`,
+            height_in_cms: `${28 * Quantity}`,
+            warehouse_id: "589",
+            movement_type: "Forward",
+            store_id: "59",
+            courier_id: "",
+            custom_awb_no: "",
+            service_name: "Surface",
+      };
+
+      // Make courier request
+      const response = await axios.post(
+        "https://api.tekipost.com/connect/order-ship",
+        courierData,
+        {
+          headers: {
+            Authorization: `Bearer ${encodedCredentials}`,
+          },
+        }
+      );
+
+      console.log("Order placed on tekipost", courierData);
+
+         const productmodification = await Promise.all(
+      ProductData.map(async (item) => {
+        // console.log(item)
         try {
           // Find the product by its ID
           const foundProduct = await Product.findById(item.productId);
@@ -566,83 +516,24 @@ const checkout = async (req, res) => {
 
     console.log("products updated successfully");
     // console.log(updateUserData)
-    res.status(200).json({ success: true, order });
-  } catch (error) {
-    res
-      .status(403)
-      .json({ message: "something went wrong try again", succes: false });
-  }
-};
+      // Redirect to success page
+      res.redirect(`${process.env.BASE_URL}/paymentsuccess?reference=${razorpay_payment_id}`);
 
-const pincodeData = (req, res) => {
-  const { pincode } = req.body;
-  //console.log(pincode);
-  const data = query.search(pincode);
-  // console.log(data);
-  if (data.length == 0) {
-    res.status(201).json({ success: false });
-    return;
-  }
-  res.status(200).json({ data, success: true });
-};
-
-const paymentVerification = async (req, res) => {
-  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
-    req.body;
-
-  const body = razorpay_order_id + "|" + razorpay_payment_id;
-
-  const expectedSignature = crypto
-    .createHmac("sha256", process.env.RAZORPAY_SECRET)
-    .update(body)
-    .digest("hex");
-
-  const isAuthentic = expectedSignature === razorpay_signature;
-  // console.log(isAuthentic);
-
-  if (isAuthentic) {
-    try {
-      // Create payment record
-      await Payment.create({
-        razorpay_order_id,
-        razorpay_payment_id,
-        razorpay_signature,
-      });
-
-      // Update order payment status
-      const orderData = await Order.findOneAndUpdate(
-        { razorpay_order_id: razorpay_order_id },
-        {
-          $set: {
-            PaymentStatus: "Paid",
-            paymentDetails: {
-              razorpay_order_id,
-              razorpay_payment_id,
-              razorpay_signature,
-            },
-          },
-        },
-        { new: true }
-      );
-
-      res.redirect(
-        `${process.env.BASE_URL}/paymentsuccess?reference=${razorpay_payment_id}`
-      );
     } catch (error) {
-      const orderData = await Order.findOneAndUpdate(
-        { orderId: razorpay_order_id },
+      console.error("Error processing payment:", error);
+      await Order.findOneAndUpdate(
+        { razorpay_order_id },
         { $set: { PaymentStatus: "Failed" } },
         { new: true }
       );
-      // console.error("Error processing payment:", error);
-      res
-        .status(500)
-        .json({ success: false, message: "Error processing payment" });
+      res.status(500).json({ success: false, message: "Error processing payment" });
     }
+
   } else {
     res.status(400).json({ success: false, message: "Invalid signature" });
   }
 };
+
 
 module.exports = {
   checkout,
